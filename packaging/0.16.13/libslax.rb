5d0ce4d306dae0b56bfeb76d97dcc5df8c499ecf
Phil Shafer <phil@juise.org>
2013-07-14 00:18:51 -0400

    new packaging data

--- packaging/0.16.13/libslax.rb
+++ packaging/0.16.13/libslax.rb
@@ -8,7 +8,7 @@ require 'formula'
 class Libslax < Formula
   homepage 'https://github.com/Juniper/@PACKAGE-NAME@'
   url 'https://github.com/Juniper/libslax/releases/0.16.13/libslax-0.16.13.tar.gz'
-  sha1 'b9a670e7b63767d938edb6c9854738755f204a1b'
+  sha1 'fce277ca2499b273e2b89f9f4c7a649f7e0807f2'
 
   depends_on 'libtool' => :build
 
