99884610058de999931ba5ec516b2ae6ebfdbe8e
Phil Shafer <phil@juise.org>
2016-01-21 19:22:33 -0500

    new packaging data

---
+++ packaging/0.20.1/libslax.rb
@@ -0,0 +1,27 @@
+#
+# Homebrew formula file for libslax
+# https://github.com/mxcl/homebrew
+#
+
+require 'formula'
+
+class Libslax < Formula
+  homepage 'https://github.com/Juniper/@PACKAGE-NAME@'
+  url 'https://github.com/Juniper/libslax/releases/0.20.1/libslax-0.20.1.tar.gz'
+  sha1 'cd2fcf4f842461fa9d6270d41775cfe5e296a9bf'
+
+  depends_on 'libtool' => :build
+
+  # Need newer versions of these libraries
+  if MacOS.version <= :lion
+    depends_on 'libxml2'
+    depends_on 'libxslt'
+    depends_on 'curl'
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
