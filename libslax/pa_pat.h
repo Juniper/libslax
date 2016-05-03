2473caccba39ff3c5932f4e9568892dbb48b55c1
Phil Shafer <phil@juise.org>
2016-05-03 00:57:22 -0400

    initial version

---
+++ libslax/pa_pat.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ *
+ * Phil Shafer <phil@>, April 2016
+ */
+
+#ifndef LIBSLAX_PA_PAT_H
+#define LIBSLAX_PA_PAT_H
+
+
+
+#endif /* LIBSLAX_PA_PAT_H */
