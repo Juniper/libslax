306d9de87fff0d15f63410b84c30347359565e1b
Phil Shafer <phil@juise.org>
2016-05-06 21:25:49 -0400

    fix compile errors (checkpoint)

--- libslax/pa_arb.c
+++ libslax/pa_arb.c
@@ -11,9 +11,9 @@
  * A "paged array" varient that allows arbitrary memory size allocations.
  */
 
-#if 0
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
@@ -21,69 +21,57 @@
 
 #include "slaxconfig.h"
 #include <libslax/slax.h>
-#include <libslax/paged_array.h>
+#include <libslax/pa_common.h>
+#include <libslax/pa_mmap.h>
 #include <libslax/pa_log2.h>
-#include <libslax/pa_malloc.h>
+#include <libslax/pa_arb.h>
 
-/*
- * Allocate the given number of pages from the underlaying allocator
- */
-static inline void *
-pa_mallac_page_raw (pa_malloc_t *pmp, unsigned num_pages)
+static inline unsigned
+pa_arb_slot (size_t size)
 {
-    page_array_t *pap = &pmp->pm_pa;
-    size_t size = num_pages << (PA_MALSM_ATOM_SHIFT + PA_MALSM_PAGE_SHIFT);
-
-    return pap->pa_alloc_fn(pap->pa_mem_opaque, size);
+    return (size < (1 << PA_ARB_ATOM_SHIFT))
+	? 0 : (pa_log2(size - 1) - PA_ARB_ATOM_SHIFT);
 }
 
 static inline unsigned
-pa_malloc_slot (size_t size)
+pa_arb_chunks_per_page (pa_arb_t *prp UNUSED, unsigned slot)
 {
-    return (size < (1 << PA_MALSM_ATOM_SHIFT))
-	? 0 : (pa_log2(i - 1) - PA_MALSM_ATOM_SHIFT);
-}
-
-static inline unsigned
-pa_malloc_chunks_per_page (pa_malloc_t *pmp, unsigned slot)
-{
-    return 1 << (PA_MALSM_PAGE_SIZE - (slot + PA_MALSM_ATOM_SHIFT));
+    return 1 << (PA_ARB_PAGE_SIZE - (slot + PA_ARB_ATOM_SHIFT));
 }
 
 /*
  * Convert from slot number to size; this is power of two, where
- * the first slot is an atom (PA_MALSM_ATOM_SIZE).
+ * the first slot is an atom (PA_ARB_ATOM_SIZE).
  */
 static inline size_t
-pa_malloc_slot_to_size (pa_malloc_t *pmp UNUSED, unsigned slot)
+pa_arb_slot_to_size (pa_arb_t *prp UNUSED, unsigned slot)
 {
-    return 1 << (slot + PA_MALSM_ATOM_SHIFT);
+    return 1 << (slot + PA_ARB_ATOM_SHIFT);
 }
 
 static void
-pa_malloc_make_page (pa_malloc_t *pmp, unsigned slot)
+pa_arb_make_page (pa_arb_t *prp, unsigned slot)
 {
-    size_t real_size = pa_malloc_slot_to_size(pmp, slot);
-    void *newp = pa_malloc_page_raw(pmp, 1); /* Allocate one page */
-
-    if (newp == NULL)
+    size_t real_size = pa_arb_slot_to_size(prp, slot);
+    pa_matom_t matom = pa_mmap_alloc(prp->pr_mmap, real_size);
+    if (matom == PA_NULL_ATOM)
 	return;
 
-    pa_atom_t *atomp = newp;
-    pa_malloc_header_t *pmhp;
-    unsigned chunks_per_page = pa_malloc_chunks_per_page(pmp, slot);
-    unsigned atoms_per_chunk = chunks_per_page >> PA_MALSM_ATOM_SHIFT;
-    unsigned i, imax;
+    uint8_t *newp = pa_mmap_addr(prp->pr_mmap, matom);
+    pa_arb_header_t *prhp;
+    unsigned chunks_per_page = pa_arb_chunks_per_page(prp, slot);
+    unsigned atoms_per_chunk = chunks_per_page >> PA_ARB_ATOM_SHIFT;
+    unsigned i, imax = chunks_per_page;
 
     /*
-     * Whiffle thru the page, setting up the
+     * Whiffle thru the page, setting up the chunks
      */
     for (i = 0; i < imax; i++) {
-	pmhp = (pa_malloc_header_t) &atomp[i * atoms_per_chunk];
-	pmhp->pmh_magic = PMH_MAGIC_SMALL;
-	pmhp->pmh_slot = slot;
-	pmhp->pmh_chunk = i;
-	pmhp->pmh_next_free = (i == imax - 1) ? 
+	prhp = (pa_arb_header_t *) (void *) (newp + i * atoms_per_chunk);
+	prhp->prh_magic = PRH_MAGIC_SMALL;
+	prhp->prh_slot = slot;
+	prhp->prh_chunk = i;
+	prhp->prh_next_free[0] = (i != imax - 1) ? i + 1 : PA_NULL_ATOM;
     }
 }
 
@@ -94,29 +82,30 @@ pa_malloc_make_page (pa_malloc_t *pmp, unsigned slot)
  * we'll allocate more memory for that size, putting items on the free list.
  */
 pa_atom_t
-pa_malloc (pa_malloc_t *pmp, size_t size)
+pa_arb_alloc_atom (pa_arb_t *prp, size_t size)
 {
-    pa_malloc_header_t *pmhp;
-    size_t full_size = size + sizeof(pa_malloc_header_t);
-    unsigned slot = pa_log2(full_size);
+    pa_arb_header_t *prhp;
+    size_t full_size = size + sizeof(pa_arb_header_t);
+    unsigned slot = pa_arb_slot(full_size);
 
     pa_atom_t atom = PA_NULL_ATOM;
 
-    if (slot < PA_MALSM_MAX_POW2) {
+    if (slot < PA_ARB_MAX_POW2) {
 	/* "Small"-style allocation */
-	atom = pmp->pm_infop->pa_free[slot];
+	atom = prp->pr_infop->pri_free[slot];
 	if (atom == PA_NULL_ATOM) {
-	    pa_malloc_page(pmp, slot);
-	    atom = pmp->pm_infop->pa_free[slot];
+	    pa_arb_make_page(prp, slot);
+	    atom = prp->pr_infop->pri_free[slot];
 	    if (atom == PA_NULL_ATOM)
 		return PA_NULL_ATOM;
 	}
 
-	pmhp = pa_atom_addr(&pmp->pm_pa, atom);
-	pmp->pm_infop->pa_free[slot] = pmhp->pmh_next_free;
+	prhp = pa_mmap_addr(prp->pr_mmap, atom);
+	prp->pr_infop->pri_free[slot] = prhp->prh_next_free[0];
 
-	pmhp->pmh_magic = PMH_MAGIC_SMALL;
-	pmhp->pmh_size = slot;
+	prhp->prh_magic = PRH_MAGIC_SMALL;
+	prhp->prh_slot = slot;
+	prhp->prh_atom = atom;	/* Not _sure_ I need this, but... */
 
     } else {
 	assert("pa_malloc: large size not supported (yet)");
@@ -126,43 +115,72 @@ pa_malloc (pa_malloc_t *pmp, size_t size)
 }
 
 void
-pa_free (pa_malloc_t *pmp, void *ptr)
+pa_arb_free_atom (pa_arb_t *prp, void *ptr)
 {
+    pa_arb_header_t *prhp = ptr;
+
+    prhp -= 1;
+
+    if (prhp->prh_magic != PRH_MAGIC_SMALL) {
+	pa_warning(0, "pa_arg: magic number wrong: %p (%u)",
+		   ptr, prhp->prh_magic);
+	return;
+    }
+
+    unsigned slot = prhp->prh_slot;
+    pa_atom_t atom = prhp->prh_atom;
+
+    if (slot < PA_ARB_MAX_POW2) {
+	/* "Small"-style allocation */
+	prhp->prh_next_free[0] = prp->pr_infop->pri_free[slot];
+	prp->pr_infop->pri_free[slot] = atom;
+    } else {
+	assert("pa_malloc: large size not supported (yet)");
+    }
 }
 
 void
-pa_malloc_init (pa_malloc_t *pmp,
-	 pa_base_set_func_t base_set_fn,
-	 pa_page_get_func_t page_get_fn, pa_page_set_func_t page_set_fn, 
-	 pa_alloc_func_t alloc_fn, pa_free_func_t free_fn, void *mem_opaque)
+pa_arb_init (pa_mmap_t *pmp UNUSED, pa_arb_t *prp UNUSED)
 {
-    if (alloc_fn == NULL)
-	alloc_fn = pa_default_alloc;
-
-    pa_init(&pmp->pm_pa, NULL, PA_MALSM_PAGE_SHIFT, PA_MALSM_ATOM_SIZE,
-	    PA_MALSM_MAX_ATOMS,
-		base_set_fn, page_get_fn, page_set_fn,
-		alloc_fn, free_fn, mem_opaque);
+    /* nothing */;
 }
 
-pa_malloc_t *
-pa_malloc_create (pa_base_set_func_t base_set_fn,
-	   pa_page_get_func_t page_get_fn, pa_page_set_func_t page_set_fn, 
-	   pa_alloc_func_t alloc_fn, pa_free_func_t free_fn, void *mem_opaque)
+pa_arb_t *
+pa_arb_setup (pa_mmap_t *pmp, pa_arb_info_t *prip)
 {
-    if (alloc_fn == NULL)
-	alloc_fn = pa_default_alloc;
+    pa_arb_t *prp = calloc(1, sizeof(*prp));
 
-    pa_malloc_t *pmp = alloc_fn(mem_opaque, sizeof(*pmp));
-
-    if (pmp) {
-	bzero(pmp, sizeof(*pmp));
-	pa_malloc_init(pmp,
-		base_set_fn, page_get_fn, page_set_fn,
-		alloc_fn, free_fn, mem_opaque);
+    if (prp) {
+	bzero(prp, sizeof(*prp));
+	prp->pr_infop = prip;
+	pa_arb_init(pmp, prp);
     }
 
-    return pmp;
+    return prp;
+}    
+
+pa_arb_t *
+pa_arb_open (pa_mmap_t *pmp, const char *name)
+{
+    pa_arb_info_t *prip = NULL;
+
+    if (name) {
+	prip = pa_mmap_header(pmp, name, sizeof(*prip));
+	if (prip == NULL) {
+	    pa_warning(0, "pa_arb header not found: %s", name);
+	    return NULL;
+	}
+    }
+
+    return pa_arb_setup(pmp, prip);
 }
 
-#endif
+
+void
+pa_arb_close (pa_arb_t *prp)
+{
+    pa_mmap_close(prp->pr_mmap);
+
+    free(prp);
+}
+
