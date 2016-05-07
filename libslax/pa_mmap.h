2d57b0596776a839718a158f83e26b8dae4635b2
Phil Shafer <phil@juise.org>
2016-05-06 21:26:53 -0400

    introduce "matom", a term for mmap's atoms

--- libslax/pa_mmap.h
+++ libslax/pa_mmap.h
@@ -24,14 +24,38 @@
  * _they_ can handle freeing memory within the allocator, if desired.
  */
 
-struct pa_mmap_s;
-typedef struct pa_mmap_s pa_mmap_t; /* Opaque type */
+#define PA_MMAP_ATOM_SHIFT	12
+#define PA_MMAP_ATOM_SIZE	(1ULL << PA_MMAP_ATOM_SHIFT)
+
+struct pa_mmap_info_s;
+typedef struct pa_mmap_info_s pa_mmap_info_t; /* Opaque type */
 
 typedef uint32_t pa_mmap_flags_t; /* Flag values */
 /* Flags for pa_mmap_flags_t */
 #define PMF_READ_ONLY	(1<<0)	/* Open read-only */
 
-pa_atom_t
+/*
+ * This structure defines the in-memory information needed for
+ * a mmap'd segment.  pm_addr == pm_infop, just a untyped.
+ */
+typedef struct pa_mmap_s {
+    int pm_fd;			/* File descriptor, if used */
+    pa_mmap_flags_t pm_flags;	/* Flags */
+    int pm_mmap_flags;		/* mmap flags parameter */
+    int pm_mmap_prot;		/* mmap prot parameter */
+    void *pm_addr;		/* Base memory address */
+    size_t pm_len;		/* Current mapped len */
+    pa_mmap_info_t *pm_infop;	/* Mmap segment header */
+} pa_mmap_t;
+
+static inline void *
+pa_mmap_addr (pa_mmap_t *pmp, pa_matom_t atom)
+{
+    return (atom == PA_NULL_ATOM) ? NULL
+	: pa_pointer(pmp->pm_addr, atom, PA_MMAP_ATOM_SHIFT);
+}
+
+pa_matom_t
 pa_mmap_alloc (pa_mmap_t *pmp, size_t size);
 
 void
