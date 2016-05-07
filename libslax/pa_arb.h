0d5b4de6d621cc85a0a4ee68fe66e7c5d554d275
Phil Shafer <phil@juise.org>
2016-05-06 21:25:51 -0400

    fix compile errors (checkpoint)

--- libslax/pa_arb.h
+++ libslax/pa_arb.h
@@ -11,79 +11,103 @@
  * A "paged array" varient that allows arbitrary memory size allocations.
  */
 
-#ifndef LIBSLAX_PA_MALLOC_H
-#define LIBSLAX_PA_MALLOC_H
+#ifndef LIBSLAX_PA_ARB_H
+#define LIBSLAX_PA_ARB_H
 
 /**
- * A trivial malloc library built on top of paged arrays.  The result
- * is a library that can be addressed by small integers, but allows
- * random allocations, rather than the fixed ones of paged arrays.
- * When small allocations are needed, pages are divided into chunks.
- * A simple array holds linked lists of free pages, indexed by power
- * of two, allowing easy access to suitable sized chunks.
+ * A trivial malloc-like arbitrary-sized memory allocation library
+ * built on top of the mmap allocator.  The result is a library that
+ * can be addressed by "atoms", but allows random allocations, rather
+ * than the fixed ones of paged arrays (pa_fixed).  When small
+ * allocations are needed, pages are divided into chunks.  A simple
+ * array holds linked lists of free pages, indexed by power of two,
+ * allowing easy access to suitable sized chunks.  We have an array
+ * indexed by slots, where each slot is a power of two.  We divide
+ * matoms returned by pa_mmap into "chunks", where each page holds a
+ * specific size of chunks.  When we hit a particular size, we stop
+ * subdividing and just hold full atoms of those powers of two sizes.
+ * To allocate, we unchain the next item on the free list and return
+ * it.  To free, we just add it to the chain.
  *
  * For larger allocations, allocations (rounded up to page sizes)
  * are made directly from the underlaying allocator, with a header
  * that identifies them as such.  Freed blocks are placed in linked
- * list sorted by number of pages.  No defragmentation is attempted.
+ * list sorted by number of pages.  No defragmentation is attempted (yet).
  */
 
-typedef struct pa_malloc_header_s {
-    uint16_t pmh_magic;	/* Magic constant so we know we are us */
-    uint8_t pmh_slot;	/* Slot number of in this allocation */
-    uint8_t pmh_chunk;	/* Chunk number with in the page */
-    pa_atom_t pmh_next_free[0]; /* If free, next free atom in list */
-} pa_malloc_header_t;
+typedef struct pa_arb_header_s {
+    uint16_t prh_magic;		/* Magic constant so we know we are us */
+    uint8_t prh_slot;	        /* Slot number of in this allocation */
+    uint8_t prh_chunk;		/* Chunk number with in the page */
+    pa_atom_t prh_atom;		/* This atom's number */
+    pa_atom_t prh_next_free[0]; /* If free, next free atom in list */
+} pa_arb_header_t;
 
-#define PMH_MAGIC_SMALL	0x5EA1 /* Indicates "small"-style allocation */
-#define PMH_MAGIC_LARGE	0xB161 /* Indicates "large"-style allocation */
+#define PRH_MAGIC_SMALL	0x5EA1 /* Indicates "small"-style allocation */
+#define PRH_MAGIC_LARGE	0xB161 /* Indicates "large"-style allocation */
 
-/** The largest "small" allocation */
-#define PA_MALSM_ATOM_SHIFT	4 /* 1<<4 == 16, size of atom */
-#define PA_MALSM_ATOM_SIZE	(1 << PA_MALSM_ATOM_SIZE)
-#define PA_MALSM_PAGE_SHIFT	12 /* 1<<12 == 4k atoms per page */
-#define PA_MALSM_PAGE_SIZE	(1 << PA_MALSM_SHIFT)
-#define PA_MALSM_BASE_SHIFT	12 /* 1<<12 == 4k pages */
-#define PA_MALSM_BASE_SIZE	(1 << PA_MALSM_BASE_SHIFT)
-
-#define PA_MALSM_MAX_ATOMS (1 << (PA_MALSM_PAGE_SHIFT + PA_MALSM_BASE_SHIFT))
+/** Constants for "small" allocations */
+#define PA_ARB_ATOM_SHIFT	4 /* 1<<4 == 16, size of atom */
+#define PA_ARB_ATOM_SIZE	(1 << PA_ARB_ATOM_SIZE)
+#define PA_ARB_PAGE_SHIFT	12 /* 1<<12 == 4k atoms per page */
+#define PA_ARB_PAGE_SIZE	(1 << PA_ARB_PAGE_SHIFT)
 
 /* This is the largest power of two that can be handled by "small" */
-#define PA_MALSM_MAX_POW2 (PA_MALSM_ATOM_SHIFT + PA_MALSM_PAGE_SHIFT)
+#define PA_ARB_MAX_POW2 PA_ARB_PAGE_SHIFT
 
+#if 0
 /*
  * Each page is divided into chunks, which are continquous atoms,
  * with a single atom being the smallest chunk we can allocate.
  * Each page has a set of bits in the malloc info that tell us which
- * chunks of a page are free
- *
+ * chunks of a page are free.
+ * (not implemented yet)
  */
 typedef uint16_t pa_alloc_bits_t;
+    pa_alloc_bits_t ppi_free_bits; /* Which chunks are free */
 
 typedef struct pa_page_info_s {
-    pa_alloc_bits_t ppi_free_bits; /* Which chunks are free */
+    uint16_t ppi_magic;		/* Magic number */
     uint16_t ppi_pow2;		/* What pow of 2 is this chunk */
 } pa_page_info_t;
+#endif /* 0 */
 
 /*
- * pa_malloc_info_t is the persistent information on the malloc store, in
- * contrast with pa_malloc_t which is transient.  
+ * pa_arb_info_t is the persistent information on the malloc store, in
+ * contrast with pa_arb_t which is transient.  
  */
-typedef struct pa_malloc_info_s {
-    pa_page_info_t pa_page_info[PA_MALSM_BASE_SIZE]; /* Per-page info */
-    pa_atom_t pa_free[PA_MALSM_MAX_POW2]; /* The free list */
-} pa_malloc_info_t;
+typedef struct pa_arb_info_s {
+    pa_atom_t pri_free[PA_ARB_MAX_POW2]; /* The free list */
+} pa_arb_info_t;
 
-typedef struct pa_malloc_s {
-    paged_array_t pm_pa;	/* The underlaying paged array */
-    pa_malloc_info_t pm_info;	/* Our info structure, if needed */
-    pa_malloc_info_t *pm_infop;	/* A pointer to our info structure */
-} pa_malloc_t;
+typedef struct pa_arb_s {
+    pa_mmap_t *pr_mmap;		/* Underlaying memory file */
+    pa_arb_info_t pr_info;	/* Our info structure, if needed */
+    pa_arb_info_t *pr_infop;	/* A pointer to our info structure */
+} pa_arb_t;
+
+static inline void *
+pa_arb_atom_addr (pa_arb_t *prp, pa_atom_t atom)
+{
+    return pa_mmap_addr(prp->pr_mmap, atom);
+}
 
 pa_atom_t
-pa_malloc (pa_malloc_t *pmp, size_t size);
+pa_arb_alloc_atom (pa_arb_t *prp, size_t size);
 
 void
-pa_free (pa_malloc_t *pmp, void *ptr);
+pa_arb_free_atom (pa_arb_t *prp, void *ptr);
 
-#endif /* LIBSLAX_PA_MALLOC_H */
+void
+pa_arb_init (pa_mmap_t *pmp, pa_arb_t *prp);
+
+pa_arb_t *
+pa_arb_setup (pa_mmap_t *pmp, pa_arb_info_t *prip);
+
+pa_arb_t *
+pa_arb_open (pa_mmap_t *pmp, const char *name);
+
+void
+pa_arb_close (pa_arb_t *prp);
+
+#endif /* LIBSLAX_PA_ARB_H */
