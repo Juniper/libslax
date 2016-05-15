d001cfb7aff6b74f80d7b40bbb6726fb9de37762
Phil Shafer <phil@juise.org>
2016-05-15 02:53:33 -0400

    checkpoint pa_pat work

--- tests/pa/pa_01.c
+++ tests/pa/pa_01.c
@@ -11,11 +11,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <assert.h>
-#include <stddef.h>
+#include <ctype.h>
+#include <limits.h>
 
 #include "slaxconfig.h"
 #include <libslax/slax.h>
@@ -23,124 +25,58 @@
 #include <libslax/pa_mmap.h>
 #include <libslax/pa_fixed.h>
 
-#define MAX_VAL	100
+#define TEST_PRINT_DULL
+#include "pa_main.h"
 
-typedef struct test_s {
-    unsigned t_magic;
-    pa_atom_t t_id;
-    int t_val[MAX_VAL];
-} test_t;
+pa_mmap_t *pmp;
+pa_fixed_t *pfp;
 
-int
-main (int argc UNUSED, char **argv UNUSED)
+void
+test_init (void)
 {
-    unsigned i;
-    test_t *tp;
-    pa_atom_t atom;
-    unsigned max_atoms = 1 << 14, shift = 6, count = 100, magic = 0x5e5e5e5e;
-    const char *filename = NULL;
-    int rm = 0;
+    return;
+}
 
-    for (argc = 1; argv[argc]; argc++) {
-	if (strcmp(argv[argc], "shift") == 0) {
-	    if (argv[argc + 1])
-		shift = atoi(argv[++argc]);
-	} else if (strcmp(argv[argc], "max") == 0) {
-	    if (argv[argc + 1]) 
-		max_atoms = atoi(argv[++argc]);
-	} else if (strcmp(argv[argc], "count") == 0) {
-	    if (argv[argc + 1]) 
-		count = atoi(argv[++argc]);
-	} else if (strcmp(argv[argc], "file") == 0) {
-	    if (argv[argc + 1]) 
-		filename = argv[++argc];
-	} else if (strcmp(argv[argc], "clean") == 0) {
-	    rm = 1;
-	}
-    }
-
-    if (rm && filename)
-	unlink(filename);
-
-    test_t **trec = calloc(count, sizeof(*tp));
-    if (trec == NULL)
-	return -1;
-
-    pa_mmap_t *pmp = pa_mmap_open(filename, 0, 0644);
+void
+test_open (void)
+{
+    pmp = pa_mmap_open(opt_filename, 0, 0644);
     assert(pmp != NULL);
 
-    pa_fixed_t *pfp = pa_fixed_open(pmp, "pa_01",
-				    shift, sizeof(test_t), max_atoms);
+    pfp = pa_fixed_open(pmp, "pa_01", opt_shift, opt_size, opt_max_atoms);
     assert(pfp != NULL);
+}
 
-    for (i = 0; i < count; i++) {
-	atom = pa_fixed_alloc_atom(pfp);
-	tp = pa_fixed_atom_addr(pfp, atom);
-	trec[i] = tp;
-	if (tp) {
-	    tp->t_magic = magic;
-	    tp->t_id = atom;
-	    memset(tp->t_val, -1, sizeof(tp->t_val));
-	}
-
-	printf("in %u : %u -> %p (%u)\n", i, atom, tp, pfp->pf_free);
+void
+test_alloc (unsigned slot, unsigned size UNUSED)
+{
+    pa_atom_t atom = pa_fixed_alloc_atom(pfp);
+    test_t *tp = pa_fixed_atom_addr(pfp, atom);
+    trec[slot] = tp;
+    if (tp) {
+	tp->t_magic = opt_magic;
+	tp->t_id = atom;
+	tp->t_slot = slot;
+	memset(tp->t_val, opt_value, opt_size - sizeof(*tp));
     }
 
-    for (i = 0; i < count; i++) {
-	if (trec[i] == NULL)
-	    continue;
-	if (i & 2)
-	    continue;
+    if (!opt_quiet)
+	printf("in %u : %u -> %p (%u)\n", slot, atom, tp, pfp->pf_free);
+}
 
+void
+test_free (unsigned slot)
+{
+    pa_atom_t atom = trec[slot]->t_id;
+    printf("free %u : %u -> %p (%u)\n", slot, atom, trec[slot], pfp->pf_free);
 
-	atom = trec[i]->t_id;
-	printf("free %u : %u -> %p (%u)\n", i, atom, trec[i], pfp->pf_free);
+    pa_fixed_free_atom(pfp, atom);
 
-	pa_fixed_free_atom(pfp, atom);
+    trec[slot] = NULL;
+}
 
-	trec[i] = NULL;
-    }
-
-    for (i = 0; i < count; i++) {
-	if (i & 4)
-	    continue;
-
-	if (trec[i] == NULL) {
-	    atom = pa_fixed_alloc_atom(pfp);
-	    tp = pa_fixed_atom_addr(pfp, atom);
-	    trec[i] = tp;
-	    if (tp) {
-		tp->t_magic = magic;
-		tp->t_id = atom;
-		memset(tp->t_val, -1, sizeof(tp->t_val));
-	    }
-	    printf("in2 %u : %u -> %p (%u)\n", i, atom, tp, pfp->pf_free);
-
-	} else {
-	    atom = trec[i]->t_id;
-	    printf("free2 %u : %u -> %p (%u)\n", i, atom, trec[i], pfp->pf_free);
-	    pa_fixed_free_atom(pfp, atom);
-	    trec[i] = NULL;
-	}
-
-
-    }
-
-    uint8_t *check = calloc(count, 1);
-    assert(check);
-
-    for (i = 0; i < count; i++) {
-	if (trec[i] == NULL)
-	    continue;
-
-	atom = trec[i]->t_id;
-	tp = pa_fixed_atom_addr(pfp, atom);
-	if (check[atom])
-	    printf("EVIL: dup: %u : %u -> %p (%u)\n", i, atom, tp, pfp->pf_free);
-	check[atom] = 1;
-
-	printf("out %u : %u -> %p (%u)\n", i, atom, tp, pfp->pf_free);
-    }
-
-    return 0;
+void
+test_close (void)
+{
+    pa_fixed_close(pfp);
 }
