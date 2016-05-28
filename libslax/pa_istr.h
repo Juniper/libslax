0b9887fd0e7980c7d65d4c71e58a713c2cc67257
Phil Shafer <phil@juise.org>
2016-05-28 12:05:40 -0400

    fix pa_mmap_header call

--- libslax/pa_istr.h
+++ libslax/pa_istr.h
@@ -22,40 +22,53 @@
  * In addition to variable strings, there are also a set of fixed
  * strings with fixed atom numbers, for one-character strings and
  * other common values.
+ *
+ * We use an index to give us a compact contiguous number space.  We
+ * have also support short strings, where small numbers represent
+ * strings of length zero or one.  
  */
 
 typedef pa_atom_t pa_istr_page_entry_t;
 
+typedef struct pa_istr_data_info_s {
+    pa_shift_t pid_shift;	/* Bits to shift to select the page */
+    uint8_t pid_padding;	/* Padding this by hand */
+    uint16_t pid_atom_shift;	/* Size of each atom */
+    pa_atom_t pid_max_atoms;	/* Max number of atoms */
+    pa_atom_t pid_free;		/* First atom that is free */
+    pa_atom_t pid_left;		/* Number of atoms left at free */
+    pa_matom_t pid_base; 	/* Offset of page table base (in mmap atoms) */
+} pa_istr_data_info_t;
+
+/*
+ * Our info contains both the info for our strings and the index
+ */
 typedef struct pa_istr_info_s {
-    pa_shift_t pii_shift;	/* Bits to shift to select the page */
-    uint8_t pii_padding;	/* Padding this by hand */
-    uint16_t pii_atom_shift;	/* Size of each atom */
-    pa_atom_t pii_max_atoms;	/* Max number of atoms */
-    pa_atom_t pii_free;		/* First atom that is free */
-    pa_atom_t pii_left;		/* Number of atoms left at free */
-    pa_matom_t pii_base; 	/* Offset of page table base (in mmap atoms) */
+    pa_fixed_info_t pii_index;	/* Packed array of string atoms */
+    pa_istr_data_info_t pii_data;	/* String data */
 } pa_istr_info_t;
 
 typedef struct pa_istr_s {
     pa_mmap_t *pi_mmap;		  /* Mmap pointer */
-    pa_istr_info_t pi_info_block; /* Simple block (not used for mmap) */
-    pa_istr_info_t *pi_infop;	   /* Pointer to real block */
+    pa_istr_info_t *pi_infop;	   /* Pointer to header */
+    pa_istr_data_info_t *pi_datap; /* Data header */
+    pa_fixed_t *pi_index;	   /* Index of strings */
     pa_istr_page_entry_t *pi_base; /* Base of page table */
 } pa_istr_t;
 
-/* Simplification macros, so we don't need to think about pi_infop */
-#define pi_shift	pi_infop->pii_shift
-#define pi_atom_shift	pi_infop->pii_atom_shift
-#define pi_max_atoms	pi_infop->pii_max_atoms
-#define pi_free		pi_infop->pii_free
-#define pi_left		pi_infop->pii_left
+/* Simplification macros, so we don't need to think about pi_datap */
+#define pi_shift	pi_datap->pid_shift
+#define pi_atom_shift	pi_datap->pid_atom_shift
+#define pi_max_atoms	pi_datap->pid_max_atoms
+#define pi_free		pi_datap->pid_free
+#define pi_left		pi_datap->pid_left
 
 static inline void
 pa_istr_base_set (pa_istr_t *pip, pa_matom_t matom,
 		   pa_istr_page_entry_t *base)
 {
     pip->pi_base = base;
-    pip->pi_infop->pii_base = matom;
+    pip->pi_datap->pid_base = matom;
 }
 
 /*
@@ -116,14 +129,37 @@ pa_istr_atom_addr (pa_istr_t *pip, pa_atom_t atom)
 			       pip->pi_max_atoms, atom);
 }
 
-static inline const char *
-pa_istr_atom_string (pa_istr_t *pip, pa_atom_t atom)
+static inline pa_atom_t
+pa_istr_index_to_atom (pa_istr_t *pip, pa_atom_t iatom)
 {
-    if (atom == PA_NULL_ATOM)
+    iatom -= PA_SHORT_STRINGS_MAX; /* Skip over short strings */
+
+    pa_atom_t *ap = pa_fixed_atom_addr(pip->pi_index, iatom);
+    return ap ? *ap : PA_NULL_ATOM;
+}
+
+static inline pa_atom_t
+pa_istr_atom_to_index (pa_istr_t *pip, pa_atom_t atom)
+{
+    pa_atom_t iatom = pa_fixed_alloc_atom(pip->pi_index);
+    pa_atom_t *ap = pa_fixed_atom_addr(pip->pi_index, iatom);
+    if (ap == NULL)
+	return PA_NULL_ATOM;
+
+    *ap = atom;			/* Record in index table */
+    return iatom + PA_SHORT_STRINGS_MAX;
+}
+
+static inline const char *
+pa_istr_atom_string (pa_istr_t *pip, pa_atom_t iatom)
+{
+    if (iatom == PA_NULL_ATOM)
 	return NULL;
 
-    if (atom < PA_SHORT_STRINGS_MAX)
-	return pa_short_string(atom);
+    if (iatom < PA_SHORT_STRINGS_MAX)
+	return pa_short_string(iatom);
+
+    pa_atom_t atom = pa_istr_index_to_atom(pip, iatom);
 
     return pa_istr_atom_addr(pip, atom);
 }
@@ -153,7 +189,7 @@ pa_istr_nstring (pa_istr_t *pip, const char *string, size_t len)
 	    memcpy(data, string, len);
 	    data[len] = '\0';
 	}
-	return atom;
+	return pa_istr_atom_to_index(pip, atom);
     }
 
     /* Pass it off to the real allocator */
@@ -187,7 +223,7 @@ pa_istr_init (pa_mmap_t *pmp, pa_istr_t *pip, pa_shift_t shift,
 	       uint16_t atom_shift, uint32_t max_atoms);
 
 pa_istr_t *
-pa_istr_setup (pa_mmap_t *pmp, pa_istr_info_t *piip, pa_shift_t shift,
+pa_istr_setup (pa_mmap_t *pmp, pa_istr_info_t *pidp, pa_shift_t shift,
 		uint16_t atom_shift, uint32_t max_atoms);
 
 pa_istr_t *
