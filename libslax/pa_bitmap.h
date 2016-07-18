1dab2964abc2322a5f35ea95424d79cf331a5def
Phil Shafer <phil@juise.org>
2016-07-18 04:36:27 -0400

    checkpoint bitmap and rulebook code

--- libslax/pa_bitmap.h
+++ libslax/pa_bitmap.h
@@ -19,6 +19,13 @@
  * us a large fixed chunk, which pa_fixed_t divides into small chunks,
  * which we use at a table of atoms of small chunks that hold the actual
  * bits.
+ *
+ * The formula for max bits for any PA_BITMAP_BLOCK_SHIFT is:
+ *    (((1 << shift) * NBBY) * ((1 << shift) / sizeof(pa_atom_t)))
+ * Counter that against needing 2 1<<shift chunks minimum per bitmap.
+ * "10" is our current best guess, yielding 2 million bits for 2k.
+ * Setting it to "12" yields 33.5 million bits for 8k.  For now, we
+ * hardcode this at 10.
  */
 
 #ifndef LIBSLAX_PA_BITMAP_H
@@ -27,26 +34,50 @@
 typedef pa_fixed_t pa_bitmap_t;	/* A pool of bitmaps */
 typedef uint32_t pa_bitnumber_t;  /* The bit number to test/set */
 typedef pa_atom_t pa_bitmap_id_t; /* An individual bitmap */
+typedef uint32_t pa_bitunit_t;	  /* Bit testing unit (word) */
 
+/*
+ * Blocks are the base size we allocate, for both bit contents and
+ * chunk pages.  They must be the same size.
+ */
 #define PA_BITMAP_BLOCK_SHIFT	10
-#define PA_BITMAP_BLOCK_SIZE	(1 << PA_BITMAP_BLOCK_SHIFT)
-#define PA_BITMAP_CHUNK_SHIFT	(PA_BITMAP_BLOCK_SHIFT - PA_ATOM_SHIFT)
-#define PA_BITMAP_CHUNK_SIZE	(1 << PA_BITMAP_CHUNK_SHIFT)
+#define PA_BITMAP_BLOCK_SIZE	(1U << PA_BITMAP_BLOCK_SHIFT)
 
-#define PA_BITMAP_MAX_ATOMS \
-    (1 << (NBBY + PA_BITMAP_BLOCK_SHIFT + PA_BITMAP_CHUNK_SHIFT))
+#define PA_BITMAP_CHUNK_SHIFT	(PA_BITMAP_BLOCK_SHIFT - PA_ATOM_SHIFT)
+#define PA_BITMAP_CHUNK_SIZE	(1U << PA_BITMAP_CHUNK_SHIFT)
+
+#define PA_BITMAP_BITS_PER_CHUNK (PA_BITMAP_BLOCK_SIZE * NBBY)
+#define PA_BITMAP_BITS_PER_UNIT	(sizeof(pa_bitunit_t) * NBBY)
+#define PA_BITMAP_UNITS_PER_CHUNK (PA_BITMAP_BLOCK_SIZE / sizeof(pa_bitunit_t))
+
+#define PA_BITMAP_MAX_BIT \
+    (NBBY * PA_BITMAP_BLOCK_SIZE * PA_BITMAP_CHUNK_SIZE)
+
+#define PA_BITMAP_MAX_ATOMS (1<<24) /* Mostly random number */
+
+#define PA_BITMAP_FIND_START	((1U << 31) - 1) /* Start/end marker */
+#define PA_BITMAP_FIND_DONE	(1U << 31) /* Start/end marker */
 
 static inline uint32_t
 pa_bitmap_bitnum (pa_bitmap_t *pfp UNUSED, pa_bitnumber_t num)
 {
-    return (num & (PA_BITMAP_BLOCK_SIZE - 1));
+    return (num & (PA_BITMAP_BITS_PER_UNIT - 1));
+}
+
+static inline uint32_t
+pa_bitmap_unitnum (pa_bitmap_t *pfp UNUSED, pa_bitnumber_t num)
+{
+    num /= PA_BITMAP_BITS_PER_UNIT;
+    num &= PA_BITMAP_UNITS_PER_CHUNK - 1;
+    return num;
 }
 
 static inline uint32_t
 pa_bitmap_chunknum (pa_bitmap_t *pfp UNUSED, pa_bitnumber_t num)
 {
+    num /= NBBY;		   /* Think in bytes */
     num >>= PA_BITMAP_BLOCK_SHIFT; /* Drop bit number */
-    num &= PA_BITMAP_CHUNK_SIZE;
+    num &= PA_BITMAP_CHUNK_SIZE - 1; /* Should be a noop */
     return num;
 }
 
@@ -59,7 +90,11 @@ pa_bitmap_alloc (pa_bitmap_t *pfp)
 static inline uint8_t
 pa_bitmap_test (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
 {
+    if (num >= PA_BITMAP_MAX_BIT)
+	return FALSE;
+
     uint32_t bitnum = pa_bitmap_bitnum(pfp, num);
+    uint32_t unitnum = pa_bitmap_unitnum(pfp, num);
     uint32_t chunknum = pa_bitmap_chunknum(pfp, num);
 
     pa_atom_t *chunkp = pa_fixed_atom_addr(pfp, bitmap_id);
@@ -70,18 +105,21 @@ pa_bitmap_test (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
     if (atom == PA_NULL_ATOM)
 	return FALSE;		/* Not allocated == never set */
 
-    uint8_t *data = pa_fixed_atom_addr(pfp, atom);
+    pa_bitunit_t *data = pa_fixed_atom_addr(pfp, atom);
     if (data == NULL)
 	return FALSE;		/* Should not occur */
 
-    return (data[bitnum >> NBBY] & (1 << (bitnum & (1 << NBBY))))
-	? TRUE : FALSE;
+    return (data[unitnum] & (1 << bitnum)) ? TRUE : FALSE;
 }
 
 static inline void
 pa_bitmap_set (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
 {
+    if (num >= PA_BITMAP_MAX_BIT)
+	return;
+
     uint32_t bitnum = pa_bitmap_bitnum(pfp, num);
+    uint32_t unitnum = pa_bitmap_unitnum(pfp, num);
     uint32_t chunknum = pa_bitmap_chunknum(pfp, num);
 
     pa_atom_t *chunkp = pa_fixed_atom_addr(pfp, bitmap_id);
@@ -97,17 +135,21 @@ pa_bitmap_set (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
 	chunkp[chunknum] = atom; /* Save into the chunk table */
     }
 
-    uint8_t *data = pa_fixed_atom_addr(pfp, atom);
+    pa_bitunit_t *data = pa_fixed_atom_addr(pfp, atom);
     if (data == NULL)
-	return;			/* Should not occur */
+	return;		/* Should not occur */
 
-    data[bitnum >> NBBY] |= 1 << (bitnum & ((1 << NBBY) - 1));
+    data[unitnum] |= 1 << bitnum;
 }
 
 static inline void
 pa_bitmap_clear (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
 {
+    if (num >= PA_BITMAP_MAX_BIT)
+	return;
+
     uint32_t bitnum = pa_bitmap_bitnum(pfp, num);
+    uint32_t unitnum = pa_bitmap_unitnum(pfp, num);
     uint32_t chunknum = pa_bitmap_chunknum(pfp, num);
 
     pa_atom_t *chunkp = pa_fixed_atom_addr(pfp, bitmap_id);
@@ -115,19 +157,118 @@ pa_bitmap_clear (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
 	return;			/* Internal error */
 
     pa_atom_t atom = chunkp[chunknum];
+    if (atom == PA_NULL_ATOM)
+	return;			/* Not allocated yet */
 
-    uint8_t *data = pa_fixed_atom_addr(pfp, atom);
+    pa_bitunit_t *data = pa_fixed_atom_addr(pfp, atom);
     if (data == NULL)
-	return;			/* Should not occur */
+	return;		/* Should not occur */
 
-    data[bitnum >> NBBY] &= ~(1 << (bitnum & (1 << NBBY)));
+    data[unitnum] &= ~(1 << bitnum);
+}
+
+static inline pa_bitnumber_t
+pa_bitmap_find_next (pa_bitmap_t *pfp, pa_atom_t bitmap_id, pa_bitnumber_t num)
+{
+    uint32_t bitnum, unitnum, chunknum, bitmask;
+    pa_atom_t *chunkp, atom;
+    pa_bitunit_t *data, value;
+
+    if (num == PA_BITMAP_FIND_START) {
+	num = 0;
+	bitmask = 0;
+    } else {
+	num += 1;			/* Start looking at the next bit */
+	if (num == PA_BITMAP_MAX_BIT)
+	    return PA_BITMAP_FIND_DONE;
+
+	/* We need to discard all the lower bits*/
+	bitnum = pa_bitmap_bitnum(pfp, num);
+	bitmask = (1 << bitnum) - 1;
+
+    }
+
+    /* Fetch the bitmap's chunk table */
+    chunkp = pa_fixed_atom_addr(pfp, bitmap_id);
+    if (chunkp == NULL)
+	return PA_BITMAP_FIND_DONE; /* Should not occur */
+
+
+
+ restart:
+    chunknum = pa_bitmap_chunknum(pfp, num);
+
+    atom = chunkp[chunknum];
+    if (atom == PA_NULL_ATOM) {
+	/*
+	 * Since this chunk hasn't been allocated, we know that
+	 * no bits in this range have been set, so we can skip to
+	 * the next chunk.
+	 */
+	while (++chunknum < PA_BITMAP_CHUNK_SIZE) {
+	    if (chunkp[chunknum] != PA_NULL_ATOM) {
+		num = chunknum * PA_BITMAP_BITS_PER_CHUNK;
+		goto restart;
+	    }
+	}
+
+	return PA_BITMAP_FIND_DONE; /* End of bits */
+    }
+
+    data = pa_fixed_atom_addr(pfp, atom);
+    if (data == NULL)
+	return PA_BITMAP_FIND_DONE; /* Should not occur */
+
+    bitnum = pa_bitmap_bitnum(pfp, num);
+    unitnum = pa_bitmap_unitnum(pfp, num);
+    value = data[unitnum];
+
+    /* Turn off all bits below the last one that we returned */
+    value &= ~bitmask;
+    bitmask = 0;		/* Don't repeat it next time thru */
+
+    if (value == 0) {	/* All zeros */
+	while (++unitnum < PA_BITMAP_UNITS_PER_CHUNK) {
+	    if (data[unitnum] != 0) {
+		num = (chunknum * PA_BITMAP_BITS_PER_CHUNK)
+		    + (unitnum * PA_BITMAP_BITS_PER_UNIT);
+		goto restart;
+	    }
+	}
+
+	/* Move to next chunk */
+	num = (chunknum + 1) * PA_BITMAP_BITS_PER_CHUNK;
+	goto restart;
+    }
+
+    /* Current unit has something set */
+    int found = ffs(value);
+    if (found <= 0)
+	return PA_BITMAP_FIND_DONE; /* Should not occur */
+
+    found -= 1;			/* ffs() numbers bits from 1, not 0 */
+
+    return (chunknum * PA_BITMAP_BITS_PER_CHUNK)
+	+ (unitnum * PA_BITMAP_BITS_PER_UNIT) + found;
 }
 
 static inline pa_bitmap_t *
 pa_bitmap_open (pa_mmap_t *pmp, const char *name)
 {
-    return pa_fixed_open(pmp, name, PA_BITMAP_BLOCK_SHIFT,
-			 PA_BITMAP_BLOCK_SIZE, PA_BITMAP_MAX_ATOMS);
+    pa_bitmap_t *pbp =  pa_fixed_open(pmp, name, PA_BITMAP_BLOCK_SHIFT,
+			      PA_BITMAP_BLOCK_SIZE, PA_BITMAP_MAX_ATOMS);
+
+    /* bitmaps require init-to-zero behavior */
+    if (pbp != NULL)
+	pa_fixed_set_flags(pbp, PFF_INIT_ZERO);
+
+    return pbp;
+}
+
+static inline void
+pa_bitmap_close (pa_bitmap_t *pbp)
+{
+    pa_fixed_close(pbp);
 }
 
 #endif /* LIBSLAX_PA_BITMAP_H */
