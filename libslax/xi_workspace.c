163805621bd56342206afa8d8a2d3e9806d62e00
Phil Shafer <phil@juise.org>
2016-08-01 03:53:19 -0400

    checkpoint namespace work

--- libslax/xi_workspace.c
+++ libslax/xi_workspace.c
@@ -51,31 +51,32 @@
 xi_workspace_t *
 xi_workspace_open (pa_mmap_t *pmp, const char *name)
 {
-    xi_namepool_t *names = NULL;
+    pa_istr_t *names = NULL;
+    pa_pat_t *names_index = NULL;
+    pa_fixed_t *ns_map = NULL;
+    pa_pat_t *ns_map_index = NULL;
     pa_istr_t *pip = NULL;
     pa_arb_t *pap = NULL;
     pa_pat_t *ppp = NULL;
     xi_node_t *nodep = NULL;
     pa_fixed_t *nodes = NULL;
-    pa_fixed_t *prefix_mapping = NULL;
     xi_workspace_t *workp = NULL;
     char namebuf[PA_MMAP_HEADER_NAME_LEN];
 
     /* Holds the names of our elements, attributes, etc */
     xi_mk_name(namebuf, name, "names");
-    names = xi_namepool_open(pmp, namebuf);
+    xi_namepool_open(pmp, namebuf, &names, &names_index);
     if (names == NULL)
+	goto fail;
+
+    xi_mk_name(namebuf, name, "namespaces");
+    xi_ns_open(pmp, namebuf, &ns_map, &ns_map_index);
+    if (ns_map == NULL)
 	goto fail;
 
     nodes = pa_fixed_open(pmp, xi_mk_name(namebuf, name, "nodes"), XI_SHIFT,
 			 sizeof(*nodep), XI_MAX_ATOMS);
     if (nodes == NULL)
-	goto fail;
-
-    prefix_mapping = pa_fixed_open(pmp,
-			    xi_mk_name(namebuf, name, "prefix-mapping"),
-			    XI_SHIFT, sizeof(xi_ns_map_t), XI_MAX_ATOMS);
-    if (prefix_mapping == NULL)
 	goto fail;
 
     pap = pa_arb_open(pmp, xi_mk_name(namebuf, name, "data"));
@@ -89,7 +90,9 @@ xi_workspace_open (pa_mmap_t *pmp, const char *name)
     workp->xw_mmap = pmp;
     workp->xw_nodes = nodes;
     workp->xw_names = names;
-    workp->xw_prefix_mapping = prefix_mapping;
+    workp->xw_names_index = names_index;
+    workp->xw_ns_map = ns_map;
+    workp->xw_ns_map_index = ns_map_index;
     workp->xw_textpool = pap;
 
     return workp;
@@ -103,68 +106,87 @@ xi_workspace_open (pa_mmap_t *pmp, const char *name)
 	pa_pat_close(ppp);
     if (nodes)
 	pa_fixed_close(nodes);
-    if (prefix_mapping)
-	pa_fixed_close(prefix_mapping);
     if (names)
-	xi_namepool_close(names);
+	pa_istr_close(names);
+    if (names_index)
+	pa_pat_close(names_index);
+    if (ns_map)
+	pa_fixed_close(ns_map);
+    if (ns_map_index)
+	pa_pat_close(ns_map_index);
     return NULL;
 }
 
-xi_namepool_t *
-xi_namepool_open (pa_mmap_t *pmap, const char *basename)
+void
+xi_namepool_open (pa_mmap_t *pmap, const char *basename,
+		  pa_istr_t **namesp, pa_pat_t **names_indexp)
 {
     char namebuf[PA_MMAP_HEADER_NAME_LEN];
     pa_istr_t *pip = NULL;
     pa_pat_t *ppp = NULL;
-    xi_namepool_t *pool;
 
-    /* The namepool holds the names of our elements, attributes, etc */
+    /* The name pool holds the names of our elements, attributes, etc */
     pip = pa_istr_open(pmap, xi_mk_name(namebuf, basename, "data"),
 		       XI_SHIFT, XI_ISTR_SHIFT, XI_MAX_ATOMS);
     if (pip == NULL)
-	return NULL;
+	return;
 
     ppp = pa_pat_open(pmap, xi_mk_name(namebuf, basename, "index"),
 		      pip, pa_pat_istr_key_func,
 		      PA_PAT_MAXKEY, XI_SHIFT, XI_MAX_ATOMS);
     if (ppp == NULL) {
 	pa_istr_close(pip);
-	return NULL;
+	return;
     }
 
-    pool = calloc(1, sizeof(*pool));
-    if (pool == NULL) {
-	pa_pat_close(ppp);
-	pa_istr_close(pip);
-	return NULL;
-    }
+    *namesp = pip;
+    *names_indexp = ppp;
+}
 
-    pool->xnp_names = pip;
-    pool->xnp_names_index = ppp;
-
-    return pool;
+static const uint8_t *
+xi_ns_key_func (pa_pat_t *pp, pa_pat_node_t *node)
+{
+    return pa_fixed_atom_addr(pp->pp_data, node->ppn_data);
 }
 
 void
-xi_namepool_close (xi_namepool_t *pool)
+xi_ns_open (pa_mmap_t *pmap, const char *basename,
+	    pa_fixed_t **nsp, pa_pat_t **ns_indexp)
 {
-    if (pool) {
-	pa_istr_close(pool->xnp_names);
-	pa_pat_close(pool->xnp_names_index);
-	free(pool);
+    char namebuf[PA_MMAP_HEADER_NAME_LEN];
+    pa_fixed_t *pfp = NULL;
+    pa_pat_t *ppp = NULL;
+
+    /* The ns pool holds the names of our elements, attributes, etc */
+    pfp = pa_fixed_open(pmap, xi_mk_name(namebuf, basename, "data"),
+			XI_SHIFT, sizeof(xi_ns_map_t), XI_MAX_ATOMS);
+    if (pfp == NULL)
+	return;
+
+    ppp = pa_pat_open(pmap, xi_mk_name(namebuf, basename, "index"),
+		      pfp, xi_ns_key_func,
+		      PA_PAT_MAXKEY, XI_SHIFT, XI_MAX_ATOMS);
+    if (ppp == NULL) {
+	pa_fixed_close(pfp);
+	return;
     }
+
+    *nsp = pfp;
+    *ns_indexp = ppp;
 }
 
 pa_atom_t
-xi_namepool_atom (xi_workspace_t *xwp, const char *data, int createp)
+xi_namepool_atom (xi_workspace_t *xwp, const char *data, xi_boolean_t createp)
 {
     uint16_t len = strlen(data) + 1;
-    pa_pat_t *ppp = xwp->xw_names->xnp_names_index;
+    pa_pat_t *ppp = xwp->xw_names_index;
+
     pa_atom_t atom = pa_pat_get_atom(ppp, len, data);
     if (atom == PA_NULL_ATOM && createp) {
-	atom = pa_istr_string(ppp->pp_data, data);
+	/* Allocate the name from our pool and add it to the tree */
+	atom = pa_istr_string(xwp->xw_names, data);
 	if (atom == PA_NULL_ATOM)
-	    pa_warning(0, "create key failed: %s", data);
+	    pa_warning(0, "namepool create key failed for key '%s'", data);
 	else if (!pa_pat_add(ppp, atom, len))
 	    pa_warning(0, "duplicate key: %s", data);
     }
@@ -182,7 +204,7 @@ xi_get_attrib (xi_workspace_t *xwp, xi_node_t *nodep, pa_atom_t name_atom)
     if (!(nodep->xn_flags & XNF_ATTRIBS_PRESENT))
 	return PA_NULL_ATOM;
 
-#if 0
+#if 0 /* XXX */
     if (!(nodep->xn_flags & XNF_ATTRIBS_EXTRACTED))
 	xi_node_attrib_extract(xwp, nodep);
 #endif
@@ -190,7 +212,7 @@ xi_get_attrib (xi_workspace_t *xwp, xi_node_t *nodep, pa_atom_t name_atom)
     for (node_atom = nodep->xn_contents; node_atom != PA_NULL_ATOM;
 	 node_atom = nodep->xn_next) {
 	nodep = xi_node_addr(xwp, node_atom);
-	if (node_atom == PA_NULL_ATOM)	/* Should not occur */
+	if (nodep == NULL)	/* Should not occur */
 	    break;
 
 	if (nodep->xn_depth <= depth)
@@ -206,4 +228,70 @@ xi_get_attrib (xi_workspace_t *xwp, xi_node_t *nodep, pa_atom_t name_atom)
     }
 
     return PA_NULL_ATOM;
+}
+
+/*
+ * Find the index of a given prefix-to-uri mapping.
+ *
+ * Note that we allow empty strings for either of these values, since
+ * that's how we define the current namespace (when prefix is empty)
+ * or the default namespace (when uri is empty).
+ *
+ * Note also that different return values from this do not imply
+ * different namespace, just different prefix mappings.  One can use
+ * distinct prefixes to access same namespace, like:
+ *    <a xmlns="a.men"><amen:b xmlns:amen="a.men"/></a>
+ * Retaining this information allows us to emit XML identical to the
+ * original input.  The cost is an extra lookup in xw_ns_map to see
+ * the underlaying atom numbers of the URI strings (which reside in
+ * the name pool).  Another fine engineering trade off that's such to
+ * bite me in the lower cheeks one day.
+ */
+pa_atom_t
+xi_ns_find (xi_workspace_t *xwp, const char *prefix, const char *uri,
+	    xi_boolean_t createp)
+{
+    pa_atom_t prefix_atom = PA_NULL_ATOM, uri_atom = PA_NULL_ATOM;
+
+    if (prefix != NULL && *prefix != '\0') {
+	prefix_atom = xi_namepool_atom(xwp, prefix, TRUE);
+	if (prefix_atom == PA_NULL_ATOM)
+	    return PA_NULL_ATOM;
+    }
+
+    if (uri != NULL && *uri != '\0') {
+	uri_atom = xi_namepool_atom(xwp, uri, TRUE);
+	if (uri_atom == PA_NULL_ATOM)
+	    return PA_NULL_ATOM;
+    }
+
+    pa_pat_t *ppp = xwp->xw_ns_map_index;
+    pa_fixed_t *pfp = xwp->xw_ns_map;
+    xi_ns_map_t ns = { prefix_atom, uri_atom };
+    pa_atom_t atom = pa_pat_get_atom(ppp, sizeof(ns), &ns);
+    if (atom == PA_NULL_ATOM && createp) {
+	atom = pa_fixed_alloc_atom(pfp);
+	if (atom == PA_NULL_ATOM) {
+	    pa_warning(0, "namespace create key failed for '%s%s%s'",
+		       prefix ?: "", prefix ? ":" : "", uri ?: "");
+	    return PA_NULL_ATOM;
+	}
+
+	xi_ns_map_t *nsp = pa_fixed_atom_addr(pfp, atom);
+	if (nsp == NULL)
+	    return PA_NULL_ATOM; /* Should not occur */
+
+	*nsp = ns;		/* Initialize newly allocated ns_map entry */
+
+	/* Add it to the patricia tree */
+	if (!pa_pat_add(ppp, atom, sizeof(ns))) {
+	    pa_fixed_free_atom(pfp, atom);
+	    pa_warning(0, "duplicate key failure for namespace '%s%s%s'",
+		       prefix ?: "", prefix ? ":" : "", uri ?: "");
+	    return PA_NULL_ATOM;
+	}
+    }
+
+    return atom;
+
 }
