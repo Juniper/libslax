345d6ab144d63a7fe7931d79729dcbbc2cf6794e
Phil Shafer <phil@juise.org>
2017-03-28 17:11:24 -0400

    add psu_malloc and friends; move ALLOCADUP here

--- libpsu/psualloc.h
+++ libpsu/psualloc.h
@@ -13,6 +13,7 @@
 #define LIBPSU_PSUALLOC_H
 
 #include <string.h>
+#include <stdlib.h>
 
 typedef void *(*psu_realloc_func_t)(void *, size_t);
 typedef void (*psu_free_func_t)(void *);
@@ -20,7 +21,83 @@ typedef void (*psu_free_func_t)(void *);
 extern psu_realloc_func_t psu_realloc;
 extern psu_free_func_t psu_free;
 
+/**
+ * malloc-like memory allocator of memory
+ * @param[in] size Number of bytes to allocate
+ * @return a pointer to newly allocated memory, or NULL
+ */
+static inline void *
+psu_malloc (size_t size)
+{
+    return psu_realloc(NULL, size);
+}
+
+/**
+ * calloc-like memory allocator of zeroed memory
+ * @param[in] size Number of bytes to allocate
+ * @return a pointer to newly allocated and zeroed memory, or NULL
+ */
+static inline void *
+psu_calloc (size_t size)
+{
+    void *ptr = psu_realloc(NULL, size);
+    if (ptr != NULL)
+	memset(ptr, 0, size);
+    return ptr;
+}
+
+/**
+ * Local reallocf(3) function.  Handles the case of leaking memory
+ * when realloc fails.  Instead of pushing it on the caller, we handle
+ * it ourselves.
+ */
+static inline void *
+psu_reallocf (void *ptr, size_t size)
+{
+    void *newptr = psu_realloc(ptr, size);
+
+    if (ptr != NULL && newptr == NULL)
+	free(ptr);
+
+    return newptr;
+}
+
 void
 psu_set_allocator (psu_realloc_func_t realloc_func, psu_free_func_t free_func);
+
+#ifndef HAVE_ALLOCADUP
+/*
+ * Helper function for ALLOCADUP
+ */
+static inline char *
+allocadupx (char *to, const char *from)
+{
+    if (to) /* Allow alloca to return NULL, which it won't */
+	strcpy(to, from);
+    return to;
+}
+
+/**
+ * @fn ALLOCADUP(const char *str)
+ * Returns a copy of a string, allocated on the stack. Think of it as
+ * strdup + alloca.  Cause it is.
+ *
+ * @param[in] str String to be duplicated
+ * @return String, copied to the stack
+ */
+#define ALLOCADUP(s) allocadupx((char *) alloca(strlen(s) + 1), s)
+
+/**
+ * @fn ALLOCADUPX(const char *str)
+ * Returns a copy of a string, allocated on the stack. Think of it as
+ * strdup + alloca.  Cause it is.  If the input is NULL, return NULL.
+ *
+ * @param[in] str String to be duplicated, or NULL
+ * @return String, copied to the stack, or NULL
+ */
+#define ALLOCADUPX(s) \
+    ((s) ? allocadupx((char *) alloca(strlen(s) + 1), s) : NULL)
+
+#endif /* HAVE_ALLOCADUP */
 
 #endif /* LIBPSU_PSUALLOC_H */
