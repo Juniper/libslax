204001d9d612a330b8dd08b7e3a28ed763e701d2
Phil Shafer <phil@juise.org>
2017-04-02 17:20:20 -0400

    initial version

---
+++ libxi/xiwhiffle.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ *
+ * Phil Shafer <phil@>, September 2016
+ */
+
+#ifndef LIBSLAX_XI_WHIFFLE_H
+#define LIBSLAX_XI_WHIFFLE_H
+
+void
+xi_whiffle_test (xi_parse_t *parsep);
+
+#endif /* LIBSLAX_XI_WHIFFLE_H */
