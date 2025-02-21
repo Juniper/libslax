6d0385e46c353702d7a200fba0ba2bee2c006de0
Phil Shafer <phil@juise.org>
2025-02-21 17:26:15 -0500

    update copyright date

--- tests/base/base_01.c
+++ tests/base/base_01.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2006-2025, Juniper Networks, Inc.
+ * All rights reserved.
+ * This SOFTWARE is licensed under the LICENSE provided in the
+ * ../Copyright file. By downloading, installing, copying, or otherwise
+ * using the SOFTWARE, you agree to be bound by the terms of that
+ * LICENSE.
+ */
 
 #include "bamain.h"
 
