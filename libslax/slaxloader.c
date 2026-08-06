1f59c4d10b1b46ac8ac6ebca6cce9a696fa4d515
Phil Shafer <phil@juise.org>
2026-08-06 01:34:56 -0400

    ctxt->linenumbers is deprecated

--- libslax/slaxloader.c
+++ libslax/slaxloader.c
@@ -268,8 +268,7 @@ slaxAvoidRtf (slax_data_t *sdp)
 	xmlAddChild(nodep->parent, newp);
 
 	/* Use the line number from the original node */
-	if (sdp->sd_ctxt->linenumbers)
-	    newp->line = nodep->line;
+	newp->line = nodep->line;
 
 	xmlNewProp(newp, (const xmlChar *) ATT_NAME, (const xmlChar *) name);
 
@@ -589,8 +588,7 @@ slaxHandleEltArg (slax_data_t *sdp, int var_on_stack)
     }
 
     /* Use the line number from the original node */
-    if (sdp->sd_ctxt->linenumbers)
-	varp->line = nodep->line;
+    varp->line = nodep->line;
 
     snprintf(str, sizeof(str), new_value_format, varname);
     ssp = slaxStringLiteral(str, T_BARE);
@@ -701,10 +699,12 @@ slaxLoadFile (const char *filename, FILE *file, xmlDictPtr dict, int partial)
     if (ctxt == NULL)
 	return NULL;
 
+#if 0
     /*
      * Turn on line number recording in each node
      */
     ctxt->linenumbers = 1;
+#endif
 
     if (dict) {
 	if (ctxt->dict)
@@ -779,10 +779,12 @@ slaxLoadBuffer (const char *filename, char *input,
     if (ctxt == NULL)
 	return NULL;
 
+#if 0
     /*
      * Turn on line number recording in each node
      */
     ctxt->linenumbers = 1;
+#endif
 
     if (dict) {
 	if (ctxt->dict)
