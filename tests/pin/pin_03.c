d04c8f0f0434fb35f5b38f3d949442ec2cb28284
Phil Shafer <phil@juise.org>
2026-08-09 17:17:32 -0400

    rename libxi to libpin

--- tests/pin/pin_03.c
+++ tests/pin/pin_03.c
@@ -32,12 +32,12 @@
 #include <parrotdb/paistr.h>
 #include <parrotdb/papat.h>
 #include <parrotdb/pabitmap.h>
-#include <libxi/xicommon.h>
-#include <libxi/xisource.h>
-#include <libxi/xirules.h>
-#include <libxi/xitree.h>
-#include <libxi/xiworkspace.h>
-#include <libxi/xiparse.h>
+#include <libpin/pin_common.h>
+#include <libpin/pin_source.h>
+#include <libpin/pin_rules.h>
+#include <libpin/pin_tree.h>
+#include <libpin/pin_workspace.h>
+#include <libpin/pin_parse.h>
 
 static char *
 check_arg (char *arg, const char *name)
@@ -60,7 +60,7 @@ main (int argc, char **argv)
     int opt_unescape UNUSED = 0;
     int opt_clean = 0;
     int opt_debug = 0;
-    xi_source_flags_t flags = 0;
+    pin_source_flags_t flags = 0;
 
     for (argc = 1; argv[argc]; argc++) {
 	const char *cp = argv[argc];
@@ -128,58 +128,58 @@ main (int argc, char **argv)
     pa_mmap_t *pmp = pa_mmap_open(opt_database, "xi03", 0, 0644);
     assert(pmp);
 
-    xi_workspace_t *workp = xi_workspace_open(pmp, "test");
+    pin_workspace_t *workp = pin_workspace_open(pmp, "test");
     assert(workp);
 
-    xi_parse_t *script = xi_parse_open(pmp, workp, "script",
+    pin_parse_t *script = pin_parse_open(pmp, workp, "script",
 				       opt_script, flags);
     assert(script);
 
     /* We need to save all attributes */
-    xi_parse_set_default_rule(script, XIA_SAVE_ATTRIB);
+    pin_parse_set_default_rule(script, XIA_SAVE_ATTRIB);
 
-    xi_parse(script);
+    pin_parse(script);
 
     if (opt_dump) {
 	if (!opt_quiet)
 	    psu_log_enable(1);
 
 	psu_log("# dump script");
-	xi_parse_dump(script);
+	pin_parse_dump(script);
 
 	psu_log("# emit script");
-	xi_parse_emit_xml(script, stdout);
+	pin_parse_emit_xml(script, stdout);
     }
 
     /* We prep the rulebook to build our states and rules */
-    xi_rulebook_t *rb = xi_rulebook_prep(script, "rulebook");
+    pin_rulebook_t *rb = pin_rulebook_prep(script, "rulebook");
     assert(rb);
 
-    xi_rulebook_dump(rb);
+    pin_rulebook_dump(rb);
 
-    xi_parse_t *parsep = xi_parse_open(pmp, workp, "test",
+    pin_parse_t *parsep = pin_parse_open(pmp, workp, "test",
 				       opt_filename, flags);
     assert(parsep);
 
     if (opt_debug)
-	xi_parse_flags_set(parsep, XI_PF_DEBUG);
+	pin_parse_flags_set(parsep, XI_PF_DEBUG);
 
-    xi_parse_set_rulebook(parsep, rb);
+    pin_parse_set_rulebook(parsep, rb);
 
-    xi_parse(parsep);
+    pin_parse(parsep);
 
     if (opt_dump) {
 	if (!opt_quiet)
 	    psu_log_enable(1);
 
 	psu_log("# dump output");
-	xi_parse_dump(parsep);
+	pin_parse_dump(parsep);
 
 	psu_log("# emit output");
-	xi_parse_emit_xml(parsep, stdout);
+	pin_parse_emit_xml(parsep, stdout);
     }
 
-    xi_parse_destroy(parsep);
+    pin_parse_destroy(parsep);
 
     return 0;
 }
