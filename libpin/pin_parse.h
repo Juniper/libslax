631ecd90f04e32f1acf6d4e91762b1dfe48c5284
Phil Shafer <phil@juise.org>
2026-08-09 17:17:04 -0400

    rename libxi to libpin

--- libpin/pin_parse.h
+++ libpin/pin_parse.h
@@ -12,19 +12,19 @@
 #ifndef LIBSLAX_XI_PARSE_H
 #define LIBSLAX_XI_PARSE_H
 
-typedef unsigned xi_parse_flags_t;
+typedef unsigned pin_parse_flags_t;
 
 /*
  * The state of the parser, meant to be both a handle to parsing
  * functionality as well as a means of restarting parsing.
  */
-typedef struct xi_parse_s {
-    xi_parse_flags_t xp_flags;  /* Flags for this parser */
-    xi_source_t *xp_srcp;	/* Source of incoming tokens */
-    xi_rulebook_t *xp_rulebook;	/* Current set of rules */
-    xi_rule_t xp_default_rule;	/* Default rule for parsing */
-    xi_insert_t *xp_insert;	/* Insertion point */
-} xi_parse_t;
+typedef struct pin_parse_s {
+    pin_parse_flags_t xp_flags;  /* Flags for this parser */
+    pin_source_t *xp_srcp;	/* Source of incoming tokens */
+    pin_rulebook_t *xp_rulebook;	/* Current set of rules */
+    pin_rule_t xp_default_rule;	/* Default rule for parsing */
+    pin_insert_t *xp_insert;	/* Insertion point */
+} pin_parse_t;
 
 /* Flags for xp_flags: */
 #define XI_PF_DEBUG		(1<<0) /* Make some debug output */
@@ -32,83 +32,83 @@ typedef struct xi_parse_s {
 #define XI_STATE_EOL		0 /* Indicates end-of-list/invalid state */
 #define XI_STATE_INITIAL	1 /* Initial parser state */
 
-typedef int (*xi_parse_emit_fn)(xi_parse_t *, xi_node_type_t,
-				xi_node_id_t node_atom, xi_node_t *,
+typedef int (*pin_parse_emit_fn)(pin_parse_t *, pin_node_type_t,
+				pin_node_id_t node_atom, pin_node_t *,
 				const char *, void *);
 
-static inline xi_parse_flags_t
-xi_parse_flags_get (xi_parse_t *pxp)
+static inline pin_parse_flags_t
+pin_parse_flags_get (pin_parse_t *pxp)
 {
     return pxp->xp_flags;
 }
 
 static inline int
-xi_parse_flags_isset (xi_parse_t *pxp, xi_parse_flags_t flags)
+pin_parse_flags_isset (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
     return (pxp->xp_flags & flags) ? 1 : 0;
 }
 
 static inline void
-xi_parse_flags_set (xi_parse_t *pxp, xi_parse_flags_t flags)
+pin_parse_flags_set (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
     pxp->xp_flags |= flags;
 }
 
 static inline void
-xi_parse_flags_clear (xi_parse_t *pxp, xi_parse_flags_t flags)
+pin_parse_flags_clear (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
     pxp->xp_flags &= ~flags;
 }
 
-static inline xi_rstate_t *
-xi_parse_stack_state (xi_parse_t *parsep)
+static inline pin_rstate_t *
+pin_parse_stack_state (pin_parse_t *parsep)
 {
-    xi_insert_t *xip = parsep->xp_insert;
-    return xip->xi_stack[xip->xi_depth].xs_statep;
+    pin_insert_t *xip = parsep->xp_insert;
+    return xip->pin_stack[xip->pin_depth].xs_statep;
 }
 
-xi_parse_t *
-xi_parse_open (pa_mmap_t *pmap, xi_workspace_t *xwp, const char *name,
-	       const char *filename, xi_source_flags_t flags);
+pin_parse_t *
+pin_parse_open (pa_mmap_t *pmap, pin_workspace_t *xwp, const char *name,
+	       const char *filename, pin_source_flags_t flags);
 
 void
-xi_parse_destroy (xi_parse_t *parsep);
+pin_parse_destroy (pin_parse_t *parsep);
 
 int
-xi_parse (xi_parse_t *parsep);
+pin_parse (pin_parse_t *parsep);
 
 void
-xi_parse_dump (xi_parse_t *parsep);
+pin_parse_dump (pin_parse_t *parsep);
 
 void
-xi_parse_emit (xi_parse_t *parsep, xi_parse_emit_fn func, void *opaque);
+pin_parse_emit (pin_parse_t *parsep, pin_parse_emit_fn func, void *opaque);
 
 void
-xi_parse_emit_xml (xi_parse_t *parsep, FILE *out);
+pin_parse_emit_xml (pin_parse_t *parsep, FILE *out);
 
 void
-xi_parse_set_rulebook (xi_parse_t *parsep, xi_rulebook_t *rulebook);
+pin_parse_set_rulebook (pin_parse_t *parsep, pin_rulebook_t *rulebook);
 
 void
-xi_parse_set_default_rule (xi_parse_t *parsep, xi_action_type_t type);
+pin_parse_set_default_rule (pin_parse_t *parsep, pin_action_type_t type);
 
-static inline xi_workspace_t *
-xi_parse_workspace (xi_parse_t *parsep)
+static inline pin_workspace_t *
+pin_parse_workspace (pin_parse_t *parsep)
 {
-    return parsep->xp_insert->xi_tree->xt_workspace;
+    return parsep->xp_insert->pin_tree->xt_workspace;
 }
 
 pa_atom_t
-xi_parse_namepool_atom (xi_parse_t *parsep, const char *name);
+pin_parse_namepool_atom (pin_parse_t *parsep, const char *name);
 
 const char *
-xi_parse_namepool_string (xi_parse_t *parsep, pa_atom_t atom);
+pin_parse_namepool_string (pin_parse_t *parsep, pa_atom_t atom);
 
 void
-xi_parse_set_rulebook (xi_parse_t *parsep, xi_rulebook_t *rulebook);
+pin_parse_set_rulebook (pin_parse_t *parsep, pin_rulebook_t *rulebook);
 
 void
-xi_node_dump (xi_workspace_t *xwp, xi_node_type_t op,
-	      xi_node_t *nodep, xi_node_id_t atom);
+pin_node_dump (pin_workspace_t *xwp, pin_node_type_t op,
+	      pin_node_t *nodep, pin_node_id_t atom);
 
 #endif /* LIBSLAX_XI_PARSE_H */
