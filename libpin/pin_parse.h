a5cbeebc2f1b6864f7f776aac4c3110195059bcc
Phil Shafer <phil@juise.org>
2026-08-09 20:46:46 -0400

    rename libxi to libpin: rename #defines and struct members

--- libpin/pin_parse.h
+++ libpin/pin_parse.h
@@ -9,8 +9,8 @@
  * Phil Shafer (phil@) June 2016
  */
 
-#ifndef LIBSLAX_XI_PARSE_H
-#define LIBSLAX_XI_PARSE_H
+#ifndef LIBSLAX_PIN_PARSE_H
+#define LIBSLAX_PIN_PARSE_H
 
 typedef unsigned pin_parse_flags_t;
 
@@ -19,18 +19,18 @@ typedef unsigned pin_parse_flags_t;
  * functionality as well as a means of restarting parsing.
  */
 typedef struct pin_parse_s {
-    pin_parse_flags_t xp_flags;  /* Flags for this parser */
-    pin_source_t *xp_srcp;	/* Source of incoming tokens */
-    pin_rulebook_t *xp_rulebook;	/* Current set of rules */
-    pin_rule_t xp_default_rule;	/* Default rule for parsing */
-    pin_insert_t *xp_insert;	/* Insertion point */
+    pin_parse_flags_t pp_flags;  /* Flags for this parser */
+    pin_source_t *pp_srcp;	/* Source of incoming tokens */
+    pin_rulebook_t *pp_rulebook;	/* Current set of rules */
+    pin_rule_t pp_default_rule;	/* Default rule for parsing */
+    pin_insert_t *pp_insert;	/* Insertion point */
 } pin_parse_t;
 
-/* Flags for xp_flags: */
-#define XI_PF_DEBUG		(1<<0) /* Make some debug output */
+/* Flags for pp_flags: */
+#define PIN_PF_DEBUG		(1<<0) /* Make some debug output */
 
-#define XI_STATE_EOL		0 /* Indicates end-of-list/invalid state */
-#define XI_STATE_INITIAL	1 /* Initial parser state */
+#define PIN_STATE_EOL		0 /* Indicates end-of-list/invalid state */
+#define PIN_STATE_INITIAL	1 /* Initial parser state */
 
 typedef int (*pin_parse_emit_fn)(pin_parse_t *, pin_node_type_t,
 				pin_node_id_t node_atom, pin_node_t *,
@@ -39,36 +39,36 @@ typedef int (*pin_parse_emit_fn)(pin_parse_t *, pin_node_type_t,
 static inline pin_parse_flags_t
 pin_parse_flags_get (pin_parse_t *pxp)
 {
-    return pxp->xp_flags;
+    return pxp->pp_flags;
 }
 
 static inline int
 pin_parse_flags_isset (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
-    return (pxp->xp_flags & flags) ? 1 : 0;
+    return (pxp->pp_flags & flags) ? 1 : 0;
 }
 
 static inline void
 pin_parse_flags_set (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
-    pxp->xp_flags |= flags;
+    pxp->pp_flags |= flags;
 }
 
 static inline void
 pin_parse_flags_clear (pin_parse_t *pxp, pin_parse_flags_t flags)
 {
-    pxp->xp_flags &= ~flags;
+    pxp->pp_flags &= ~flags;
 }
 
 static inline pin_rstate_t *
 pin_parse_stack_state (pin_parse_t *parsep)
 {
-    pin_insert_t *xip = parsep->xp_insert;
-    return xip->pin_stack[xip->pin_depth].xs_statep;
+    pin_insert_t *pip = parsep->pp_insert;
+    return pip->pin_stack[pip->pin_depth].ps_statep;
 }
 
 pin_parse_t *
-pin_parse_open (pa_mmap_t *pmap, pin_workspace_t *xwp, const char *name,
+pin_parse_open (pa_mmap_t *pmap, pin_workspace_t *pwp, const char *name,
 	       const char *filename, pin_source_flags_t flags);
 
 void
@@ -95,7 +95,7 @@ pin_parse_set_default_rule (pin_parse_t *parsep, pin_action_type_t type);
 static inline pin_workspace_t *
 pin_parse_workspace (pin_parse_t *parsep)
 {
-    return parsep->xp_insert->pin_tree->xt_workspace;
+    return parsep->pp_insert->pin_tree->pt_workspace;
 }
 
 pa_atom_t
@@ -108,7 +108,7 @@ void
 pin_parse_set_rulebook (pin_parse_t *parsep, pin_rulebook_t *rulebook);
 
 void
-pin_node_dump (pin_workspace_t *xwp, pin_node_type_t op,
+pin_node_dump (pin_workspace_t *pwp, pin_node_type_t op,
 	      pin_node_t *nodep, pin_node_id_t atom);
 
-#endif /* LIBSLAX_XI_PARSE_H */
+#endif /* LIBSLAX_PIN_PARSE_H */
