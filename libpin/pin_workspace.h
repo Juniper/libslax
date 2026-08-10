0c7bbcf8b765d7d307ef728834026f300057f5ed
Phil Shafer <phil@juise.org>
2026-08-09 20:46:58 -0400

    rename libxi to libpin: rename #defines and struct members

--- libpin/pin_workspace.h
+++ libpin/pin_workspace.h
@@ -13,8 +13,8 @@
  * built, allowing multiple trees (documents) to share.
  */
 
-#ifndef LIBSLAX_XI_WORKSPACE_H
-#define LIBSLAX_XI_WORKSPACE_H
+#ifndef LIBSLAX_PIN_WORKSPACE_H
+#define LIBSLAX_PIN_WORKSPACE_H
 
 #include <parrotdb/pafixed.h>
 #include <parrotdb/paarb.h>
@@ -23,15 +23,15 @@
 #include <libpin/pin_node.h>
 
 typedef struct pin_workspace_s {
-    pa_mmap_t *xw_mmap;	/* Base memory information */
-    pa_fixed_t *xw_nodes;	/* Pool of nodes (pin_node_t) */
-    pa_istr_t *xw_names;	/* Array of names (element, attr, etc) */
-    pa_pat_t *xw_names_index;	/* Patricia tree for names */
-    pa_fixed_t *xw_ns_map; /* Map from prefixes to URLs (pin_ns_map_t) */
-    pa_pat_t *xw_ns_map_index;	/* Index of xw_ns_map entries */
-    pa_arb_t *xw_textpool;	/* Text data values */
-    pa_fixed_t *xw_nodeset_chunks; /* Pool of chunks for nodesets node lists */
-    pa_fixed_t *xw_nodeset_info; /* Pool of chunks for nodeset "info" data */
+    pa_mmap_t *pw_mmap;	/* Base memory information */
+    pa_fixed_t *pw_nodes;	/* Pool of nodes (pin_node_t) */
+    pa_istr_t *pw_names;	/* Array of names (element, attr, etc) */
+    pa_pat_t *pw_names_index;	/* Patricia tree for names */
+    pa_fixed_t *pw_ns_map; /* Map from prefixes to URLs (pin_ns_map_t) */
+    pa_pat_t *pw_ns_map_index;	/* Index of pw_ns_map entries */
+    pa_arb_t *pw_textpool;	/* Text data values */
+    pa_fixed_t *pw_nodeset_chunks; /* Pool of chunks for nodesets node lists */
+    pa_fixed_t *pw_nodeset_info; /* Pool of chunks for nodeset "info" data */
 } pin_workspace_t;
 
 pin_workspace_t *
@@ -46,38 +46,38 @@ pin_ns_open (pa_mmap_t *pmap, const char *basename,
 	    pa_fixed_t **nsp, pa_pat_t **ns_indexp);
 
 pin_ns_map_id_t
-pin_ns_find (pin_workspace_t *xwp, const char *prefix, const char *uri,
+pin_ns_find (pin_workspace_t *pwp, const char *prefix, const char *uri,
 	    pin_boolean_t createp);
 
 #include "gen/pin_node_gen.h"
 
 pa_atom_t
-pin_namepool_atom (pin_workspace_t *xwp, const char *data, pin_boolean_t createp);
+pin_namepool_atom (pin_workspace_t *pwp, const char *data, pin_boolean_t createp);
 
 static inline const char *
-pin_namepool_string (pin_workspace_t *xwp, pa_atom_t name_atom)
+pin_namepool_string (pin_workspace_t *pwp, pa_atom_t name_atom)
 {
-    return pa_istr_atom_string(xwp->xw_names, pa_istr_atom(name_atom));
+    return pa_istr_atom_string(pwp->pw_names, pa_istr_atom(name_atom));
 }
 
 pa_atom_t
-pin_get_attrib (pin_workspace_t *xwp, pin_node_t *nodep, pa_atom_t name_atom);
+pin_get_attrib (pin_workspace_t *pwp, pin_node_t *nodep, pa_atom_t name_atom);
 
 static inline const char *
-pin_textpool_string (pin_workspace_t *xwp, pa_atom_t atom)
+pin_textpool_string (pin_workspace_t *pwp, pa_atom_t atom)
 {
-    return pa_arb_atom_addr(xwp->xw_textpool, pa_arb_atom(atom));
+    return pa_arb_atom_addr(pwp->pw_textpool, pa_arb_atom(atom));
 }
 
 static inline const char *
-pin_get_attrib_string (pin_workspace_t *xwp, pin_node_t *nodep,
+pin_get_attrib_string (pin_workspace_t *pwp, pin_node_t *nodep,
 		      pa_atom_t name_atom)
 {
-    pa_atom_t atom = pin_get_attrib(xwp, nodep, name_atom);
-    return (atom == PA_NULL_ATOM) ? NULL : pin_textpool_string(xwp, atom);
+    pa_atom_t atom = pin_get_attrib(pwp, nodep, name_atom);
+    return (atom == PA_NULL_ATOM) ? NULL : pin_textpool_string(pwp, atom);
 }
 
 #include "gen/pin_ns_map_gen.h"
 
-#endif /* LIBSLAX_XI_WORKSPACE_H */
+#endif /* LIBSLAX_PIN_WORKSPACE_H */
 
