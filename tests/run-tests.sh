18677856920ec402156c02238e271030dea0ab05
Phil Shafer <phil@juise.org>
2026-09-15 12:48:21 -0400

    run () {     if [ ! -z "${GT_TRACE}" ]; then         echo + "$@";     fi;     "$@"; }

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -129,6 +129,87 @@ do_accept () {
 }
 
 #
+# Run a set of pre-converted .xsl files against their matching .xml inputs.
+#
+# Each .xsl is named test-{base}-{NN}.xsl; the XML input is
+# ${BASEDIR}/test-{base}.xml (the trailing -NN is stripped to find it).
+# We use pin_08.test as the runner since it accepts an XSLT file directly
+# via "input <xsl>" and an XML document via "xml <xml>".
+#
+# Output lands in out/core/; saved references live in ${SRCDIR}/saved/core/.
+#
+do_run_slax () {
+    mkdir -p out/core
+    absbasedir=`cd "${BASEDIR}" && pwd`
+
+    for file in ${TESTS}; do
+	EXT=`echo $file | sed -E 's:.*\.([^\./]*)$:\1:'`
+
+	base=`basename $file .$EXT`
+
+	basedata=`echo $base | sed 's/-[0-9]*$//'`
+	xmlfile=${absbasedir}/${basedata}.xml
+
+	if [ ! -f "$xmlfile" ]; then
+	    info "... skipping $base (no xml: $xmlfile)"
+	    continue
+	fi
+
+	oname=core/${base}
+	out=out/${oname}
+
+	${ECHO} -n "... ${base} ..."
+	run "./pin_08.test input ${file} xml ${xmlfile} quiet dump clean \
+	    > ${out}.out 2> ${out}.err"
+	${ECHO} "    done"
+
+	run "diff -Nu ${SRCDIR}/saved/${oname}.out ${out}.out | ${S2O}"
+	run "diff -Nu ${SRCDIR}/saved/${oname}.err ${out}.err | ${S2O}"
+    done
+}
+
+do_accept_slax () {
+    mkdir -p ${SRCDIR}/saved/core
+
+    for file in ${TESTS}; do
+	base=`basename $file .xsl`
+	oname=core/${base}
+
+	accept_file out/${oname}.out ${SRCDIR}/saved/${oname}.out
+	accept_file out/${oname}.err ${SRCDIR}/saved/${oname}.err
+    done
+}
+
+do_run_one_slax () {
+    local slaxproc=${SLAXPROC:-../../slaxproc/slaxproc}
+    for file in "$@"; do
+	EXT=`echo $file | sed -E 's:.*\.([^\./]*)$:\1:'`
+
+	base=`basename $file .EXT`
+
+	basedata=`echo $base | sed 's/-[0-9]*$//'`
+	xmlfile=${BASEDIR}/${basedata}.xml
+
+	oname=core/${base}
+	out=${OUTDIR:-out}/${oname}
+	rfile=${file}
+
+	if [ "$EXT" = "slax" ]; then
+	    run "$slaxproc -x $file > $out.xsl"
+	    rfile=$out.xsl
+	fi
+
+	${ECHO} -n "... ${base} ..."
+	run "./pin_08.test input ${tfile} xml ${xmlfile} quiet dump clean \
+	    > ${out}.out 2> ${out}.err"
+	${ECHO} "    done"
+
+	run "diff -Nu ${SRCDIR}/saved/${oname}.out ${out}.out | ${S2O}"
+	run "diff -Nu ${SRCDIR}/saved/${oname}.err ${out}.err | ${S2O}"
+    done
+}
+
+#
 # pa and xi tests do not work on linux yet
 #
 case `uname`-`basename $PWD` in
@@ -138,7 +219,10 @@ esac
 while [ $# -gt 0 ]
 do
     case "$1" in
+    -b) BASEDIR=$2; shift;;
     -d) SRCDIR=$2; shift;;
+    -o) OUTDIR=$2; shift;;
+    -s) SLAXPROC=$2; shift;;
     -v) S2O=cat;;
     -*) echo "unknown option" >&2; exit;;
     *) break;;
@@ -167,6 +251,20 @@ case $verb in
     accept-all)
         TESTS=`echo *test`
         do_accept
+    ;;
+
+    run-one-slax)
+        do_run_one_slax "$@"
+    ;;
+
+    run-slax)
+        TESTS="$@"
+        do_run_slax
+    ;;
+
+    accept-slax)
+        TESTS="$@"
+        do_accept_slax
     ;;
 
     *)
